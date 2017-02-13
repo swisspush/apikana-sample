33cfa292a0ed5a1ad9b01f1a24554ec887d1406b
nidi3 <ich@nidi.guru>
2017-02-13 16:32:58 +0100

    use apikana parent-pom

--- maven/apikana-sample-dependent/src/model/ts/user.ts
+++ maven/apikana-sample-dependent/src/model/ts/user.ts
@@ -1,8 +1,8 @@
 import {Company} from "apikana-sample-base/company";
-import {Int, TimeOnly,DateOnly, DateTime, UTC} from "apikana/default-types";
+// import {Int, TimeOnly,DateOnly, DateTime, UTC} from "apikana/default-types";
 export type UUID=string // @format uuid
 interface Users {
-    count: Int
+    // count: Int
     data: User[]
 }
 
@@ -11,15 +11,15 @@ enum Gender{
 }
 
 interface User {
-    id: UUID
+    // id: UUID
     firstName: string //the given name
     lastName: string //the family name
-    age?: Int
+    // age?: Int
 
-    birthday: DateOnly
-    birthtime: TimeOnly
-    birth: DateTime
-    birthinstant: UTC
+    // birthday: DateOnly
+    // birthtime: TimeOnly
+    // birth: DateTime
+    // birthinstant: UTC
     parent?: User
     company: Company
 }
